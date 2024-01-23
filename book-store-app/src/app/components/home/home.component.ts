200f85bee480002b33479a28ad12ef61a4a84542
Jeff Anderson <jeffacdev@gmail.com>
2024-01-23 15:50:41 -0600

    component state example

--- book-store-app/src/app/components/home/home.component.ts
+++ book-store-app/src/app/components/home/home.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  showMore: boolean = false;
+  showMoreBtnTxt: string = 'Show more..';
 
+  toggleShowMore() {
+    this.showMore = !this.showMore;
+    this.showMoreBtnTxt = this.showMore ? 'Show less..' : 'Show more..';
+  }
 }
